c2f2dce97e2ef7856947e625382c22dea5fb3be1
vkyip <thomas@radicasoftware.com>
2021-05-12 10:46:02 +0800

    Update plugin_0.js

--- plugin_0.js
+++ plugin_0.js
@@ -1,5 +1,4 @@
 //https://github.com/vecta-io/plugins/blob/main/plugin_0.js
-try{
 // console.log('Hello world');
 
 var shape = Vecta.activePage.drawRect(50, 50, 800, 100);
@@ -7,5 +6,3 @@ var shape = Vecta.activePage.drawRect(50, 50, 800, 100);
 shape.fill('#66ccff');
 shape.text('Hello ' +  $.doc().creator() + '!');
 shape.fontSize(60);
-}catch(e){console.warn("Error found in plugin Hello world");console.error(e); }
-Vecta.trigger("pluginStopped.Vecta.Plugin");
