acc0da37c746c5c7040e25fba02d7a30438bdc80
vkyip <thomas@radicasoftware.com>
2021-05-12 17:37:16 +0800

    Update plugin_0.js

--- plugin_0.js
+++
@@ -1,8 +0,0 @@
-//https://github.com/vecta-io/plugins/blob/main/plugin_0.js
-// console.log('Hello world');
-
-var shape = Vecta.activePage.drawRect(50, 50, 800, 100);
-
-shape.fill('#66ccff');
-shape.text('Hello ' +  $.doc().creator() + '!');
-shape.fontSize(60);
